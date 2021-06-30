c9599c86d89efcd4c160e4e585daba53f232ad92
Mark <mark3fullen@gmail.com>
2021-06-30 01:01:04 -0400

    commit tree 681366c45e875ca5ec74fd506327d97bd9465ba9

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = (list) => {
+    return list.slice(0,2);
+}
+
+const returnLastTwoDrivers = (list) => {
+    return list.slice(2,4);
+} 
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(num) {
+    return function() {
+        let fare
+        return fare = (num * 5);
+    }
+}
+
+const fareDoubler = (time) => {
+    let fare
+    return fare = (time*2);
+}
+
+const fareTripler = (time) => {
+    let fare
+    return fare = time*3;
+}
+
+function selectDifferentDrivers(list, func) {
+    return func(list)
+}
